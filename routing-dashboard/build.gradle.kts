c24304cefbb4d66f7e922dc144b2dfebbe67c35d
Chris Stelzmüller <tuesd4y@protonmail.ch>
2022-01-18 20:46:51 +0100

    add docker configuration for routing-backend

--- routing-dashboard/build.gradle.kts
+++ routing-dashboard/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 	kotlin("plugin.spring") version "1.6.10"
 }
 
-group = "com.tuesd4y"
-version = "0.0.1-SNAPSHOT"
+group = "com.tuesd4y.routingdashboard"
+version = "1.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
